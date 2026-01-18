5bb219e458ebd4dc6cf46d6db08fc8dace69cda4
Neyser Novoa <nenriquenoli@gmail.com>
2026-01-18 03:26:20 -0500

    week 02 completed

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+let currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("header nav");
+const hamburgerSymbol = "☰";
+const closeSymbol = "✖"; 
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+
+  if (nav.classList.contains("open")) {
+    hamburger.textContent = closeSymbol;
+  } else {
+    hamburger.textContent = hamburgerSymbol;
+  }
+});
